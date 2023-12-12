5ee5632ea8aba420061a4e38a70126dab32d4fc6
SimonMachira <simonmachira@gmail.com>
2023-12-12 11:51:36 -0800

    main.h

--- main.h
+++ main.h
@@ -54,17 +54,34 @@ void free_stat_buff(buff_list **head);
 char *buff_set(char *k, char c, unsigned int b);
 buff_list *add_buff(buff_list **head, char *pointer);
 void buff_free(buff_list **head);
+char **strn_tokn(char *tokn);
+size_t tokn_ln(char *strn, size_t spec);
+size_t tokn_cnt(char *strn);
+void rec_sig(int sign);
+void rec_sig(int sign __attribute__((unused)));
+ssize_t buff_cp(char **app_str, char *res_str, ssize_t *start);
+void shft_bf(char *buf, ssize_t q);
+ssize_t get_line(char **mem);
+size_t strn_len(const char *strng);
+char **gen_path(char *const *arg);
+size_t path_avail(char *const *arg);
+size_t arg_len(char ptr_path);
+size_t num_path(char *ar_path);
 
 /*external variables go here*/
 
 /*execution prototypes go here*/
 int process_builtin(char *const *arg);
 int ext_command(char *const *arg);
-int cd_dir(char *const *arg)
-
-
+int cd_dir(char *const *arg);
+int main(int argc __attribute__((unused)), char **arg);
+int buff_alloc(char *buf);
+int _strcomp(const char *strn1, const char *strn2);
+int strn_compl(const char *strn1, const char *strn2, size_t q);
 char *_atoi(size_t num, char *buff, int tok);
 size_t digit_count(size_t digit);
 int _stoi(char *str);
+int PWD_set(char *var_v);
+int oldPWD_set(void);
 
 #endif
