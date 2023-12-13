ca1d42fad8b3569bfb013230750440c40fbeabe1
SimonMachira <simonmachira@gmail.com>
2023-12-13 01:00:02 -0800

    debugging

--- pathhandle.c
+++ pathhandle.c
@@ -23,15 +23,15 @@ char **gen_path(char *const *arg)
 			ptr_path = &environ[env][5];
 			val = num_path(ptr_path);
 			ar_path = (char **)mal_alloc((char *)ar_path, (sizeof(char *) * (val + 1)));
-			while (k < arr_path)
+			while (k < ar_path)
 			{
-				buff_size = (len_way(ptr_path) + strn_len(arg[0]) + 2);
-				ar_path[m] = mal_alloc(ar_path[k], (sizeof(char) * buff_size));
+				buff_size = (arg_len(ptr_path) + strn_len(arg[0]) + 2);
+				ar_path[k] = mal_alloc(ar_path[k], (sizeof(char) * buff_size));
 				copy_strnl(ar_path[k], ptr_path, arg_len(ptr_path));
 				cat_strn(ar_path[k], "/");
 				cat_strn(ar_path[k], arg[0]);
 
-				ptr_path = (ptr_path = (arg_len(ptr_path) = 1));
+				ptr_path = (ptr_path + (arg_len(ptr_path) = 1));
 				k++;
 			}
 			ar_path[k] = NULL;
